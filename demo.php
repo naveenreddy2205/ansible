8bb10305b1922f3ba2226506f71f15c42835b675
root <root@ip-172-31-84-231.ec2.internal>
2020-11-28 15:27:53 +0000

    ansible scripts

---
+++ demo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World to demo of php!";
+echo "New Update!";
+?> 
+
+</body>
+</html>
+
---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My dummy first PHP page</h1>
+
+<?php
+echo "Hello World!";
+echo "New Update!";
+?> 
+
+</body>
+</html>
+
